95e21996923c5783b0b1cd7c1e2b0f74221c6f74
Wang Yeming <wangyeming@dotide.com>
2014-10-17 21:15:40 +0800

    增加setting的listview

--- TinySouAndroid/app/src/main/java/com/example/freestorm/tinysouandroid/settingActivity.java
+++ TinySouAndroid/app/src/main/java/com/example/freestorm/tinysouandroid/settingActivity.java
@@ -4,14 +4,25 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class settingActivity extends Activity {
+    private CheckBox cb1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        cb1 = (CheckBox) findViewById(R.id.chech_box1);
+        listViewInit();
     }
 
 
@@ -32,5 +43,17 @@ public class settingActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void listViewInit(){
+        ListView listView = (ListView) findViewById(R.id.listview1);
+        List<Map<String, Object>> listItem = new ArrayList<Map<String, Object>>();
+        Map<String, Object> map1 = new HashMap<String, Object>();
+        map1.put("TITLE", getString(R.string.setting1));
+        map1.put("CHECKBOX", cb1);
+        listItem.add(map1);
+        SimpleAdapter adapter = new SimpleAdapter(this, listItem,
+                R.layout.check_list, new String[]{"TITLE","CHECKBOX"}, new int[]{R.id.item_title, R.id.chech_box1} );
+        listView.setAdapter(adapter);
     }
 }
